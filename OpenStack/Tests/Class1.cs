6df43a48c81151d3d0c62d5cd6c425ce12588e97
Nuno Cancelo <nuno.cancelo@gmail.com>
2014-07-08 21:28:37 +0100

    Examples of OpenStack interaction.

---
+++ OpenStack/Tests/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Configuration;
+
+namespace Tests
+{
+    public class Class1
+    {
+
+
+        public static void Main()
+        {
+            var authenticationUrl = ConfigurationManager.AppSettings["identityApiTokensURL"];
+            var username = ConfigurationManager.AppSettings["username"];
+            var  password = ConfigurationManager.AppSettings["password"];
+            var  tenantId = ConfigurationManager.AppSettings["tenantId"];
+            var request = WebRequest.Create(authenticationUrl);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+
+            string authString = "{\"auth\":{"
+            + "\"passwordCredentials\":{"
+            + "\"username\":\"" + username + "\","
+            + "\"password\":\"" + password + "\""
+            + "},\"tenantName\":\"" + tenantId + "\"}}";
+
+            var encoding = new ASCIIEncoding();
+            var bytes = encoding.GetBytes(authString);
+            
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+
+            var response = request.GetResponse();
+            string token;
+            if (response != null)
+            {
+                string jsonResponse;
+                using (var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    jsonResponse = sr.ReadToEnd();
+                }
+                
+            }
+        }
+
+    }
+}
